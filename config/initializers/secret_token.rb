845b585cbd5861857d29f6d502663862ed61ae8e
Eric Cantor <eric@mobileglobal.com>
2013-10-15 17:03:27 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classy::Application.config.secret_token = '31471da3fea286df3920dda5153de964dd20f4ee6e822de7bf9497b57639427990ec26e4e10a1cfc96383cc9aa51013c889408ea0f5e266b2398babc84cc8d84'
